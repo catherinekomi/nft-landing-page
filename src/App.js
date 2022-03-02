440f55013c5067573d9c1976459491cca9d966b6
bob <oceanbeaats@gmail.com>
2022-03-02 14:26:48 -0800

    scss files added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React from 'react';
+import Clients from './components/Clients';
+import Free from './components/Free';
+import Home from './components/Home';
+import Like from './components/Like';
+import Navbar from './components/Navbar';
+import Release from './components/Release';
+import ScrollToTop from './components/ScrollToTop';
+import Signup from './components/Signup';
+import SuperRare from './components/SuperRare';
 
 export default function App() {
   return (
-    <div>Hello</div>
+    <div className='app-container'>
+      <ScrollToTop/>
+      <Navbar />
+      <Home/>
+      <Free/>
+      <Clients/>
+      <SuperRare/>
+      <Release/>
+      <Like/>
+      <Signup/>
+
+    </div>
   )
 }
 
